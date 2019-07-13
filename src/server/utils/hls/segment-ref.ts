6f6ea5fcb9582591096656ab6e9b5c57f49bfd6b
f0reachARR <f0reach@f0reach.me>
2019-07-13 22:19:03 +0900

    Use packet number as segment ref

--- src/server/utils/hls/segment-ref.ts
+++ src/server/utils/hls/segment-ref.ts
@@ -4,14 +4,16 @@ import { base58 } from '../base58';
 
 const segBufSecret = createHash('md5')
   .update(config.secrets.segmentRef)
-  .digest();
+  .digest()
+  .slice(0, 8);
 
 export const encodeSegmentRef = (offset: number, length: number) => {
-  const buf = Buffer.alloc(12);
-  buf.writeUIntBE(offset, 0, 6);
-  buf.writeUIntBE(length, 6, 6);
+  const buf = Buffer.alloc(8);
+  buf.writeUIntBE(offset / 188, 0, 4);
+  buf.writeUIntBE(length / 188, 4, 4);
 
-  const cipher = createCipheriv('aes-128-ecb', segBufSecret, '');
+  const cipher = createCipheriv('bf-ecb', segBufSecret, '');
+  cipher.setAutoPadding(false);
   const encrypted = Buffer.concat([cipher.update(buf), cipher.final()]);
 
   return base58.encode(encrypted);
@@ -20,10 +22,11 @@ export const encodeSegmentRef = (offset: number, length: number) => {
 export const decodeSegmentRef = (refText: string) => {
   const encrypted = base58.decode(refText);
 
-  const decipher = createDecipheriv('aes-128-ecb', segBufSecret, '');
+  const decipher = createDecipheriv('bf-ecb', segBufSecret, '');
+  decipher.setAutoPadding(false);
   const buf = Buffer.concat([decipher.update(encrypted), decipher.final()]);
 
-  const offset = buf.readUIntBE(0, 6);
-  const length = buf.readUIntBE(6, 6);
+  const offset = buf.readUIntBE(0, 4) * 188;
+  const length = buf.readUIntBE(4, 4) * 188;
   return { offset, length };
 };
