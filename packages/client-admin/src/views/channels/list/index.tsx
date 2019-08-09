0028f2849f9686e6f08739bf1c817a524972c198
f0reachARR <f0reach@f0reach.me>
2019-08-09 22:18:13 +0900

    Add infinite scroll to channel list

--- packages/client-admin/src/views/channels/list/index.tsx
+++ packages/client-admin/src/views/channels/list/index.tsx
@@ -7,23 +7,56 @@ import Typography from '@material-ui/core/Typography';
 import { Add } from '@material-ui/icons';
 import { PageContainer, useErrorSnack } from '@mitei/client-common';
 import * as React from 'react';
+import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
 import { useGetChannelsSimpleQuery } from '../../../api/generated/graphql';
 import { AddChannelDialog } from '../../../components/channels/AddChannelDialog';
 import { useCommonStyles } from '../../../styles/common';
 
 export const ChannelsListView = () => {
-  const { data, error, loading, refetch } = useGetChannelsSimpleQuery({
+  const {
+    data,
+    error,
+    loading,
+    refetch,
+    fetchMore,
+  } = useGetChannelsSimpleQuery({
     errorPolicy: 'all',
     variables: { skip: 0, take: 10 },
   });
   const openErrorMessage = useErrorSnack();
   const commonStyles = useCommonStyles();
   const [addDialogOpen, setAddDialogOpen] = React.useState(false);
+  const [scrollRef, inView] = useInView();
+
+  React.useEffect(() => {
+    if (inView && !loading && data) {
+      const { channels } = data.channelList;
+      fetchMore({
+        variables: {
+          skip: channels.length,
+          take: 10,
+        },
+        updateQuery(prev, { fetchMoreResult }) {
+          if (!fetchMoreResult) return prev;
+          return {
+            channelList: {
+              total: fetchMoreResult.channelList.total,
+              channels: [
+                ...prev.channelList.channels,
+                ...fetchMoreResult.channelList.channels,
+              ],
+              __typename: prev.channelList.__typename,
+            },
+          };
+        },
+      });
+    }
+  }, [inView]);
 
   if (loading) return <CircularProgress />;
 
-  if (error || !data || !data.channelList) {
+  if (error || !data) {
     openErrorMessage(error ? error.message : '一覧の取得に失敗');
     return null;
   }
@@ -38,11 +71,13 @@ export const ChannelsListView = () => {
     setAddDialogOpen(true);
   };
 
+  const { total, channels } = data.channelList;
+  const hasMore = total > channels.length;
   return (
     <PageContainer title='チャンネル一覧'>
-      <Typography>{data.channelList.total} 件</Typography>
+      <Typography>{total} 件</Typography>
       <List>
-        {data.channelList.channels.map(channel => {
+        {channels.map(channel => {
           return (
             <ListItem
               key={channel.id}
@@ -57,6 +92,7 @@ export const ChannelsListView = () => {
             </ListItem>
           );
         })}
+        {hasMore && <div ref={scrollRef} />}
       </List>
       <AddChannelDialog open={addDialogOpen} handleClose={handleDialogClose} />
       <Fab
