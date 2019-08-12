7287f2a658f9f314b0d5d185b0374338b6f663d7
f0reachARR <f0reach@f0reach.me>
2019-08-13 00:22:16 +0900

    Add source api

--- packages/server-media/src/api/types/source-reference.ts
+++ packages/server-media/src/api/types/source-reference.ts
@@ -1,18 +1,22 @@
 import { SourceReferenceResolvers } from '../../generated/graphql';
-import { FileSource } from '../../models/FileSource';
-import { RecordSource } from '../../models/RecordSource';
 import { RtmpInput } from '../../models/RtmpInput';
+import {
+  TranscodedSource,
+  TranscodedSourceDocument,
+} from '../../models/TranscodedSource';
 
 export const sourceReferenceResolvers: SourceReferenceResolvers = {
   __resolveType: sourceRef => {
     if (!('collection' in sourceRef)) throw new Error();
     switch (sourceRef.collection.name) {
+      case TranscodedSource.collection.name:
+        if ((sourceRef as TranscodedSourceDocument).type === 'record') {
+          return 'RecordSource';
+        } else {
+          return 'FileSource';
+        }
       case RtmpInput.collection.name:
         return 'RtmpInput';
-      case FileSource.collection.name:
-        return 'FileSource';
-      case RecordSource.collection.name:
-        return 'RecordSource';
       default:
         throw new Error();
     }
