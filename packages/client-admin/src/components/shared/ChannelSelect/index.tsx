c975336f788cb397adbc978aa3968999c8996162
f0reachARR <f0reach@f0reach.me>
2019-08-13 21:45:02 +0900

    Add channel select component

---
+++ packages/client-admin/src/components/shared/ChannelSelect/index.tsx
@@ -0,0 +1,93 @@
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/core/styles';
+import * as React from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useGetChannelsSimpleQuery } from '../../../api/generated/graphql';
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+  list: {
+    maxHeight: 300,
+  },
+});
+
+type Props = {
+  value: string;
+  handleChange: (value: string) => void;
+  disabled?: boolean;
+};
+export const ChannelSelect = (props: Props) => {
+  const styles = useStyles();
+  const { data, loading, fetchMore } = useGetChannelsSimpleQuery({
+    variables: { skip: 0, take: 10 },
+    errorPolicy: 'all',
+  });
+  const [scrollRef, inView] = useInView();
+
+  React.useEffect(() => {
+    if (inView && !loading && data) {
+      const { channels } = data.channelList;
+      fetchMore({
+        variables: {
+          skip: channels.length,
+          take: 10,
+        },
+        updateQuery(prev, { fetchMoreResult }) {
+          if (!fetchMoreResult) return prev;
+          return {
+            channelList: {
+              total: fetchMoreResult.channelList.total,
+              channels: [
+                ...prev.channelList.channels,
+                ...fetchMoreResult.channelList.channels,
+              ],
+              __typename: prev.channelList.__typename,
+            },
+          };
+        },
+      });
+    }
+  }, [inView]);
+
+  const handleChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    props.handleChange(e.target.value as string);
+  };
+
+  const total = data && data.channelList ? data.channelList.total : 0;
+  const channels = data && data.channelList ? data.channelList.channels : [];
+  const hasMore = total > channels.length;
+  return (
+    <FormControl className={styles.root} disabled={props.disabled}>
+      <InputLabel htmlFor='channel'>チャンネル選択</InputLabel>
+      <Select
+        value={props.value}
+        onChange={handleChange}
+        inputProps={{
+          id: 'channel',
+        }}
+        disabled={(loading && total === 0) || props.disabled}
+        MenuProps={{
+          MenuListProps: {
+            className: styles.list,
+          },
+        }}
+      >
+        {channels.map(({ id, displayName }) => (
+          <MenuItem value={id} key={id}>
+            {displayName}
+          </MenuItem>
+        ))}
+        {hasMore && (
+          <MenuItem disabled innerRef={scrollRef}>
+            Loading
+          </MenuItem>
+        )}
+      </Select>
+    </FormControl>
+  );
+};
