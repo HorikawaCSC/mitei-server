92ddc821f67a3f308c8262a1eabbb974712ddc23
f0reachARR <f0reach@f0reach.me>
2019-08-13 21:20:59 +0900

    Update schedule query resolver

--- packages/server-media/src/api/queries/streaming/schedule.ts
+++ packages/server-media/src/api/queries/streaming/schedule.ts
@@ -1,17 +1,33 @@
 import { QueryResolvers } from '../../../generated/graphql';
 import { Schedule } from '../../../models/streaming/Schedule';
+import { omitUndefined } from '../../../utils/db';
 import { ensureLoggedInAsAdmin } from '../../../utils/gql/ensureUser';
 
 export const scheduleQueryResolvers: QueryResolvers = {
-  scheduleList: ensureLoggedInAsAdmin(async (_parent, { take, skip }) => {
-    const total = await Schedule.countDocuments();
-    const result = await Schedule.find()
-      .skip(skip || 0)
-      .limit(take || 10);
+  scheduleList: ensureLoggedInAsAdmin(
+    async (_parent, { take, skip, startAt, endAt, ...query }) => {
+      const conditions = omitUndefined(query) as Record<string, unknown>;
 
-    return {
-      schedules: result,
-      total,
-    };
+      if (startAt) {
+        conditions.endAt = { $gte: startAt };
+      }
+
+      if (endAt) {
+        conditions.startAt = { $lte: endAt };
+      }
+
+      const total = await Schedule.countDocuments(conditions);
+      const result = await Schedule.find(conditions)
+        .skip(skip || 0)
+        .limit(take || 10);
+
+      return {
+        schedules: result,
+        total,
+      };
+    },
+  ),
+  schedule: ensureLoggedInAsAdmin(async (_parent, { id }) => {
+    return (await Schedule.findById(id)) || null;
   }),
 };
