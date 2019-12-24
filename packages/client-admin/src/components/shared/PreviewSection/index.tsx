3b63d0d0d7b3c23573795cb2f5a5f59164ce1a00
f0reachARR <f0reach@f0reach.me>
2019-12-24 13:43:02 +0900

    Update PreviewSection

--- packages/client-admin/src/components/shared/PreviewSection/index.tsx
+++ packages/client-admin/src/components/shared/PreviewSection/index.tsx
@@ -1,3 +1,5 @@
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { HLSPlayer, PageContainer } from '@mitei/client-common';
 import * as React from 'react';
@@ -14,10 +16,22 @@ type Props = {
 
 export const PreviewSection = ({ source }: Props) => {
   const styles = useStyles();
+  const [show, setShow] = React.useState(false);
+  const toggleShow = React.useCallback(() => setShow(!show), [show]);
 
   return (
     <PageContainer title='プレビュー' mini>
-      <HLSPlayer source={source} controls className={styles.video} />
+      <Button onClick={toggleShow}>{show ? '非表示' : '表示'}</Button>
+      {show && (
+        <Box>
+          <HLSPlayer
+            source={source}
+            controls
+            className={styles.video}
+            autoFix
+          />
+        </Box>
+      )}
     </PageContainer>
   );
 };
