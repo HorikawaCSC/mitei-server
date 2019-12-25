42da177c17b02c0179ace33733df7568b9a902c4
f0reachARR <f0reach@f0reach.me>
2019-12-25 22:25:33 +0900

    Allow any origin in streaming api

--- packages/server-media/src/routes/api/index.ts
+++ packages/server-media/src/routes/api/index.ts
@@ -6,6 +6,11 @@ import { router as tsRouter } from './stream/segment';
 
 export const router = Router();
 
+router.use((_req, res, next) => {
+  res.setHeader('access-control-allow-origin', '*');
+  next();
+});
+
 router.use('/', tsRouter);
 router.use('/source', sourceRouter);
 router.use('/channel', channelRouter);
