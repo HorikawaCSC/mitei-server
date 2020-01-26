27deb640ee758276bd448bf01ecb1366e4a5be66
f0reachARR <f0reach@f0reach.me>
2020-01-26 19:28:35 +0900

    Add autoplay failure control

--- packages/client-viewer/src/components/ChannelPlayer/index.tsx
+++ packages/client-viewer/src/components/ChannelPlayer/index.tsx
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { HLSPlayer } from '@mitei/client-common';
+import { HLSPlayer, useErrorSnack } from '@mitei/client-common';
 import * as React from 'react';
 import { useChannelPlay } from '../../utils/channel';
 import { isDebug } from '../../utils/config';
@@ -19,37 +19,51 @@ type Props = {
 export const ChannelPlayer = ({ channelId }: Props) => {
   const [scheduleAvailable, remain] = useChannelPlay(channelId);
   const [playing, setPlaying] = React.useState(false);
+  const [showControls, setShowControls] = React.useState(isDebug);
   const styles = useStyles();
+  const showSnack = useErrorSnack();
 
-  const handlePlay = () => {
+  const handlePlay = React.useCallback(() => {
     setPlaying(true);
+    setShowControls(false);
     console.log('play', scheduleAvailable, remain);
-  };
-  const handlePlayEnd = () => {
+  }, [scheduleAvailable, remain]);
+
+  const handlePlayEnd = React.useCallback(() => {
     setPlaying(false);
     console.log('play end', scheduleAvailable, remain);
-  };
-  const handleStall = () => {
+  }, [scheduleAvailable, remain]);
+
+  const handleStall = React.useCallback(() => {
     console.log('stall', scheduleAvailable, remain);
     if (!scheduleAvailable) {
       setPlaying(false);
     }
-  };
+  }, [scheduleAvailable, remain]);
+
+  const handleAutoplayFail = React.useCallback(() => {
+    showSnack('自動再生に失敗しました。手動で再生を開始してください。');
+    setShowControls(true);
+  }, []);
+
   const playable = React.useMemo(() => (playing ? true : scheduleAvailable), [
     scheduleAvailable,
     playing,
   ]);
-  console.log('playable', playable);
+
+  if (isDebug) console.log('playable', playable);
+
   return playable ? (
     <HLSPlayer
       onPlay={handlePlay}
       onEnded={handlePlayEnd}
-      controls={isDebug}
+      controls={showControls}
       source={channelManifestUrl(channelId)}
       autoplay
       className={styles.video}
       autoFix
       onStallBuffer={handleStall}
+      onAutoPlayFailure={handleAutoplayFail}
     />
   ) : (
     <WaitingView />
