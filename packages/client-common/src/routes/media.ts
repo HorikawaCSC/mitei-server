5ca3f2d41f487e84a083850929558e79eb3570b2
f0reachARR <f0reach@f0reach.me>
2020-02-04 15:05:57 +0900

    Add license for client-common

--- packages/client-common/src/routes/media.ts
+++ packages/client-common/src/routes/media.ts
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Mitei Server.
+ * Copyright (c) 2019 f0reachARR <f0reach@f0reach.me>
+ *
+ * Mitei Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Mitei Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mitei Server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 export const mediaRoutes = {
   channel: (name: string) => `/api/channel/${name}/manifest.m3u8`,
   source: (name: string) => `/api/source/${name}/manifest.m3u8`,
