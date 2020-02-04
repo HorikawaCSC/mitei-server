230e4b73fee50f717e103bedd60458635a6f2e21
f0reachARR <f0reach@f0reach.me>
2020-02-04 12:45:12 +0900

    Make ProgramListSection item simple

--- packages/client-admin/src/components/schedules/ProgramListSection/item.tsx
+++ packages/client-admin/src/components/schedules/ProgramListSection/item.tsx
@@ -1,13 +1,19 @@
 import { ExecutionResult } from '@apollo/react-common';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import { Delete, Save } from '@material-ui/icons';
+import {
+  Create,
+  Delete,
+  DragIndicator,
+  Save,
+  SettingsBackupRestore,
+} from '@material-ui/icons';
 import {
   useConfirmDialog,
   useErrorDialog,
@@ -23,6 +29,7 @@ import {
   useUpdateProgramMutation,
 } from '../../../api/generated/graphql';
 import { useCommonStyles } from '../../../styles/common';
+import { programText } from '../../../utils/schedule';
 import { RtmpInputSelect } from '../../shared/RtmpInputSelect';
 import { SourceSelect } from '../../shared/SourceSelect';
 import { SourceTypeSelect } from '../../shared/SourceTypeSelect';
@@ -40,6 +47,15 @@ const useStyles = makeStyles(theme =>
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
+    },
+    titleBox: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    titleText: {
+      display: 'flex',
+      alignItems: 'center',
+      paddingRight: '10px',
     },
   }),
 );
@@ -74,12 +90,13 @@ export const ProgramItem = ({
     program.source ? program.source.id : '',
   );
   const [duration, setDuration] = React.useState(program.duration);
+  const [open, setOpen] = React.useState(false);
 
   React.useEffect(() => {
     if (programType === ProgramType.Transcoded) {
       getSourceDuration({ variables: { id: sourceId } });
     }
-  }, [programType, sourceId]);
+  }, [programType, sourceId, getSourceDuration]);
 
   const handleApplyTimeFromSource = React.useCallback(() => {
     if (
@@ -118,6 +135,12 @@ export const ProgramItem = ({
     [],
   );
 
+  const handleReset = React.useCallback(() => {
+    setProgramType(program.type);
+    setSourceId(program.source ? program.source.id : '');
+    setDuration(program.duration);
+  }, [program]);
+
   const handleSave = React.useCallback(async () => {
     const { errors } = (await updateProgram({
       variables: {
@@ -130,13 +153,21 @@ export const ProgramItem = ({
     })) as ExecutionResult<{}>;
     if (errors) {
       showError(errors[0].message);
-      setProgramType(program.type);
-      setSourceId(program.source ? program.source.id : '');
-      setDuration(program.duration);
+      handleReset();
     } else {
       showMessage('保存しました');
     }
-  }, [schedule, program, duration, programType, sourceId]);
+  }, [
+    schedule,
+    program,
+    duration,
+    programType,
+    sourceId,
+    handleReset,
+    showError,
+    showMessage,
+    updateProgram,
+  ]);
 
   const handleRemove = React.useCallback(async () => {
     if (!(await confirm('確認', '削除しますか'))) return;
@@ -151,7 +182,19 @@ export const ProgramItem = ({
     } else {
       showMessage('削除しました');
     }
-  }, [schedule, program]);
+  }, [schedule, program, showError, showMessage, removeProgram, confirm]);
+
+  const handleOpen = React.useCallback(() => {
+    if (open) {
+      handleSave();
+    }
+    setOpen(!open);
+  }, [open, handleSave]);
+
+  const handleResetButton = React.useCallback(() => {
+    handleReset();
+    setOpen(false);
+  }, [handleReset]);
 
   const sourceSelector = React.useMemo(
     () =>
@@ -206,62 +249,71 @@ export const ProgramItem = ({
         <div {...provided.draggableProps} ref={provided.innerRef}>
           <Paper className={styles.paper}>
             <Box className={styles.title}>
-              <Typography {...provided.dragHandleProps} variant='h6'>
-                プログラム #{index}
-              </Typography>
+              <Box {...provided.dragHandleProps} className={styles.titleBox}>
+                <Typography className={styles.titleText} variant='h6'>
+                  <DragIndicator />
+                  プログラム #{index}
+                </Typography>
+                <Typography variant='subtitle2'>
+                  {programText(program)}
+                </Typography>
+              </Box>
               <Box>
+                <IconButton disabled={disabled} onClick={handleOpen} edge='end'>
+                  {open ? <Save /> : <Create />}
+                </IconButton>
+                {open && (
+                  <IconButton
+                    disabled={disabled}
+                    onClick={handleResetButton}
+                    edge='end'
+                  >
+                    <SettingsBackupRestore />
+                  </IconButton>
+                )}
                 <IconButton
                   onClick={handleRemove}
-                  disabled={disabled}
+                  disabled={!saveEnable || disabled}
                   edge='end'
                 >
                   <Delete />
                 </IconButton>
               </Box>
             </Box>
-            <Box className={commonStyles.centerBox}>
-              <SourceTypeSelect
-                value={programType}
-                onChange={handleChangeProgramType}
-                disabled={disabled}
-              />
-              {sourceSelector}
-            </Box>
-            <Box className={commonStyles.centerBox}>
-              <TextField
-                label={`長さ(秒 / 最大: ${maxDuration})`}
-                type='number'
-                value={duration}
-                error={duration > maxDuration}
-                onChange={handleDurationChange}
-                disabled={disabled}
-              />
-              <Button
-                disabled={!timeAppliable || disabled}
-                onClick={handleApplyTimeFromSource}
-                color='secondary'
-              >
-                長さをソースから適用
-              </Button>
-              <Button
-                onClick={handleApplyTimeFromMax}
-                color='secondary'
-                disabled={disabled}
-              >
-                長さを最大に
-              </Button>
-            </Box>
-            <ButtonGroup
-              color='primary'
-              variant='contained'
-              className={styles.buttonGroup}
-              disabled={disabled}
-            >
-              <Button onClick={handleSave} disabled={!saveEnable || disabled}>
-                <Save />
-                保存
-              </Button>
-            </ButtonGroup>
+            <Collapse in={open} timeout='auto'>
+              <Box className={commonStyles.centerBox}>
+                <SourceTypeSelect
+                  value={programType}
+                  onChange={handleChangeProgramType}
+                  disabled={disabled}
+                />
+                {sourceSelector}
+              </Box>
+              <Box className={commonStyles.centerBox}>
+                <TextField
+                  label={`長さ(秒 / 最大: ${maxDuration})`}
+                  type='number'
+                  value={duration}
+                  error={duration > maxDuration}
+                  onChange={handleDurationChange}
+                  disabled={disabled}
+                />
+                <Button
+                  disabled={!timeAppliable || disabled}
+                  onClick={handleApplyTimeFromSource}
+                  color='secondary'
+                >
+                  長さをソースから適用
+                </Button>
+                <Button
+                  onClick={handleApplyTimeFromMax}
+                  color='secondary'
+                  disabled={disabled}
+                >
+                  長さを最大に
+                </Button>
+              </Box>
+            </Collapse>
           </Paper>
         </div>
       )}
