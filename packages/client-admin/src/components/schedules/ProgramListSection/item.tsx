4a5b7849be1e8b5e1ef4f04eb0c220e7c8242673
f0reachARR <f0reach@f0reach.me>
2020-02-04 15:16:51 +0900

    Add license for client-admin

--- packages/client-admin/src/components/schedules/ProgramListSection/item.tsx
+++ packages/client-admin/src/components/schedules/ProgramListSection/item.tsx
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Mitei Server.
+ * Copyright (c) 2019 f0reachARR <f0reach@f0reach.me>
+ *
+ * Mitei Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Mitei Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mitei Server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { ExecutionResult } from '@apollo/react-common';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
