3a5f2ad17613a5695e9f1fdde9c481387cfa5083
f0reachARR <f0reach@f0reach.me>
2020-02-05 21:31:46 +0900

    Add channel name update UI

--- packages/client-admin/src/components/channels/ChannelInfoSection/index.tsx
+++ packages/client-admin/src/components/channels/ChannelInfoSection/index.tsx
@@ -17,10 +17,13 @@
 
 import { ExecutionResult } from '@apollo/react-common';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Edit } from '@material-ui/icons';
 import {
   PageContainer,
   useErrorDialog,
+  useErrorSnack,
   useMessageSnack,
 } from '@mitei/client-common';
 import * as React from 'react';
@@ -28,7 +31,9 @@ import {
   FillerControl,
   GetChannelDetailQuery,
   useUpdateChannelFillerControlMutation,
+  useUpdateChannelNameMutation,
 } from '../../../api/generated/graphql';
+import { RenameSourceDialog } from '../../shared/RenameDialog';
 import { FillerModeSelect } from '../FillerModeSelect';
 
 export const ChannelInfoSection = ({
@@ -45,7 +50,17 @@ export const ChannelInfoSection = ({
   const [updating, setUpdating] = React.useState(false);
   const showMessage = useMessageSnack();
   const showErrorDialog = useErrorDialog();
+  const [
+    renameChannel,
+    { loading: renameLoading },
+  ] = useUpdateChannelNameMutation({
+    errorPolicy: 'all',
+  });
+  const [renameDialogOpen, setRenameDialogOpen] = React.useState(false);
+
   React.useEffect(() => setFillerControl(channel.fillerControl), [channel]);
+
+  const showErrorSnack = useErrorSnack();
 
   const handleFillerControlChange = async (value: FillerControl) => {
     setUpdating(true);
@@ -61,19 +76,57 @@ export const ChannelInfoSection = ({
     setUpdating(false);
   };
 
+  const handleShowRenameDialog = React.useCallback(() => {
+    setRenameDialogOpen(true);
+  }, [setRenameDialogOpen]);
+
+  const handleCloseRenameDialog = React.useCallback(() => {
+    setRenameDialogOpen(false);
+  }, [setRenameDialogOpen]);
+
+  const handleRename = React.useCallback(
+    async (name: string) => {
+      const { errors } = await renameChannel({
+        variables: {
+          id: channel.id,
+          name,
+        },
+      });
+
+      if (errors) {
+        showErrorSnack(errors[0].message);
+        throw errors[0];
+      }
+    },
+    [renameChannel, channel],
+  );
+
   return (
-    <PageContainer title={`チャンネル: ${channel.displayName}`}>
-      <Typography component='p'>ID: {channel.id}</Typography>
-      <Box>
-        <Typography component='p'>
-          フィラー配信モード (変更はすぐに反映されません):
-        </Typography>
-        <FillerModeSelect
-          value={fillerControl}
-          onChange={handleFillerControlChange}
-          disabled={updating}
-        />
-      </Box>
-    </PageContainer>
+    <>
+      <PageContainer title={`チャンネル: ${channel.displayName}`}>
+        <Typography component='p'>ID: {channel.id}</Typography>
+        <Box>
+          <Typography component='p'>
+            フィラー配信モード (変更はすぐに反映されません):
+          </Typography>
+          <FillerModeSelect
+            value={fillerControl}
+            onChange={handleFillerControlChange}
+            disabled={updating}
+          />
+        </Box>
+        <Button color='secondary' onClick={handleShowRenameDialog}>
+          <Edit />
+          <Typography component='span'>名前の変更</Typography>
+        </Button>
+      </PageContainer>
+      <RenameSourceDialog
+        open={renameDialogOpen}
+        loading={renameLoading}
+        onClose={handleCloseRenameDialog}
+        onApply={handleRename}
+        defaultName={channel.displayName}
+      />
+    </>
   );
 };
