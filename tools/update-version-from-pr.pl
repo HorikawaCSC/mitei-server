35249db1c1f1ae10b746becef4ac4e5773188866
f0reachARR <f0reach@f0reach.me>
2020-02-18 17:47:42 +0900

    Fix flow 7

--- tools/update-version-from-pr.pl
+++ tools/update-version-from-pr.pl
@@ -29,7 +29,11 @@ system("git", "checkout", "-b", "release-$version") == 0 or die;
 
 system("npx", "lerna", "version", "$version", "--yes", "--no-git-tag-version", "--no-push") == 0 or die;
 
-system("git", "tag", "-a", "v$version", "-m", "v$version") == 0 or die;
+my @tags = exec_command('git tag');
+if(scalar(grep { $_ eq "v$version" } @tags) == 0) {
+  system("git", "tag", "-a", "v$version", "-m", "v$version") == 0 or die;
+}
+
 system("git", "push", "-u", "origin", "--tags", "release-$version") == 0 or die;
 
 sub exec_command {
